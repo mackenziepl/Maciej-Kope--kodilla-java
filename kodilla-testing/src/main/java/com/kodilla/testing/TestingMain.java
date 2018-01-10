461d530d07ec81ecb50b97029f4d2f0dfd016b5f
mackenziepl <mackenziepl@poczta.onet.pl>
2018-01-10 21:05:51 +0100

    text

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,36 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        // tu umieść kolejny test ->
+        System.out.println("Test - pierwszy test jednostkowy:");
+        Calculator calculator = new Calculator(5,4);
+
+        int result1 = calculator.add(5,4);
+
+        if (result1 == 9)
+            System.out.println("test OK");
+        else
+            System.out.println("Error!");
+
+        int result2 = calculator.subtract(5,4);
+
+        if (result2 == 1)
+            System.out.println("test OK");
+        else
+            System.out.println("Error!");
     }
 }
