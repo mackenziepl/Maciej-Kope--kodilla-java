8b647324f655577106b4d41135f812c3e4dde1e0
mackenziepl <mackenziepl@poczta.onet.pl>
2018-01-20 21:13:24 +0100

    Zadanie PoemDecorator

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,12 +1,52 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.lambda.ExecuteSaySomething;
-import com.kodilla.stream.lambda.Processor;
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.*;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+import static jdk.nashorn.internal.objects.NativeString.toLowerCase;
+import static jdk.nashorn.internal.objects.NativeString.toUpperCase;
 
 public class StreamMain {
     public static void main(String[] args) {
+        System.out.println("Welcome to module 7 - Stream");
+
+        SaySomething saySomething = new SaySomething();
+        saySomething.say();
+
         Processor processor = new Processor();
         ExecuteSaySomething executeSaySomething = new ExecuteSaySomething();
         processor.execute(executeSaySomething);
+
+        Executor codeToExecute = () -> System.out.println("This is an example text.");
+        processor.execute(codeToExecute);
+
+        processor.execute(() -> System.out.println("This is an example text."));
+
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        System.out.println("Calculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+
+        poemBeautifier.beautify("Jestem wesoły Romek",text -> "ABC " + text + " ABC");
+        poemBeautifier.beautify("Jestem wesoły Romek",text -> toUpperCase(text));
+        poemBeautifier.beautify("Jestem wesoły Romek",text -> text + "...");
+        poemBeautifier.beautify("Jestem wesoły Romek",text -> toLowerCase(text));
+
+
+
     }
+
+
 }
--- kodilla-stream/src/main/java/com/kodilla/stream/lambda/SaySomething.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/lambda/SaySomething.java
@@ -2,6 +2,6 @@ package com.kodilla.stream.lambda;
 
 public class SaySomething {
     public void say() {
-            System.out.println("This is an example text.");
+        System.out.println("This is an example text.");
     }
 }
