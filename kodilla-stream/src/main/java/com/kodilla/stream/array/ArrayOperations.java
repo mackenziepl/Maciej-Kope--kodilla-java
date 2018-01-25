58001b74e8dc883928f26b83fe6c16254699cd60
mackenziepl <mackenziepl@poczta.onet.pl>
2018-01-25 21:52:12 +0100

    Zadanie: try! catch! finally!

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -0,0 +1,15 @@
+package com.kodilla.stream.array;
+
+public interface ArrayOperations {
+    /*int[] numbers = new int[20];
+    static int getAverage(int[] numbers) {
+        int number = IntStream.range(0, numbers.length)
+                //.flatMap()
+               // .map(number -> number);
+                .sum();
+                //.average();
+
+                //.forEach(System.out::println);
+
+    }*/
+}
