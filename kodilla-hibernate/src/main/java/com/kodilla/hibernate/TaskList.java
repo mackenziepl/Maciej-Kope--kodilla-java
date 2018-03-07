0d9f2111f46fdda8dd0a2ad5d8259108a6f13355
mackenziepl <mackenziepl@poczta.onet.pl>
2018-03-07 22:39:59 +0100

    Zadanie: Nagłówki faktur oraz ich pozycje

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/TaskList.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/TaskList.java
@@ -1,7 +1,11 @@
 package com.kodilla.hibernate;
 
+import com.kodilla.hibernate.task.Task;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "TASKLISTS")
@@ -9,9 +13,9 @@ public class TaskList {
     private int id;
     private String listName;
     private String description;
+    private List<Task> tasks = new ArrayList<>();
 
     public TaskList() {
-
     }
 
     public TaskList(String listName, String description) {
@@ -47,5 +51,19 @@ public class TaskList {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    @OneToMany(
+            targetEntity = Task.class,
+            mappedBy = "taskList",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
     }
 }
