6eae3e45b0722aa84dd004915295e62c5541f815
mackenziepl <mackenziepl@poczta.onet.pl>
2018-05-19 16:43:44 +0200

    Zadanie: Szukamy laptopów na eBay

--- kodilla-testing2/src/main/java/com/kodilla/testing2/ebay/EBayTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/ebay/EBayTestingApp.java
@@ -1,19 +1,19 @@
-package com.kodilla.testing2.google;
+package com.kodilla.testing2.ebay;
 
 import com.kodilla.testing2.config.WebDriverConfig;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class GoogleTestingApp {
-    public static final String SEARCHFIELD = "lst-ib";
+public class EBayTestingApp {
+    public static final String SEARCHFIELD = "gh-ac";
 
     public static void main(String[] args) {
         WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.FIREFOX);
-        driver.get("https://www.google.com");
+        driver.get("https://www.ebay.com");
 
         WebElement searchField = driver.findElement(By.id(SEARCHFIELD));
-        searchField.sendKeys("Kodilla");
+        searchField.sendKeys("Laptop");
         searchField.submit();
     }
 }
